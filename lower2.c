12de0a6db244b70d216abb2f2ac63ad78986abe6
aminos <amriamine@protonmail.com>
2018-06-15 22:28:58 +0100

    Convert char to lower without using if

---
+++ lower2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+char lower(char);
+
+int main(void)
+{
+  char c = getchar();
+  printf("c: %c, lower(%c): %c", c , c , lower(c));
+  return 0;
+}
+
+char lower(char c)
+{
+  return (c >= 'A' && c <= 'Z') ? c + 'a' - 'A' : c;
+}
