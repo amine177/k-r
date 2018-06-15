d2ed56d2ea13596a2204e2e1cc8a72cd588e9b3f
aminos <amriamine@protonmail.com>
2018-06-15 19:28:57 +0100

    Done

---
+++ invert.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+
+unsigned invert(unsigned, int, int);
+
+int main(void)
+{
+  char c = getchar();
+  int n = 4;
+  int p = 5;
+  getchar();
+  printf("c: %#x, invert(%x, %d, %d) : %#x",
+      c, c, p, n, invert(c, p, n));
+}
+
+unsigned invert(unsigned x, int p, int n)
+{
+  //or just call getbits and setbits
+  unsigned y = ~((x >> (p+1-n)) & ~(~0 << n));
+  return (x & ~(~(~0 << n) << (p+1-n))) | ((y & ~(~0 << n)) << (p+1-n));
+
+}
