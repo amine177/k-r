a41d529a9f1e943361f967a916c20059bad306f1
aminos <amriamine@protonmail.com>
2018-07-10 17:18:10 +0100

    getline_ , readlines, writelines

--- ch5/strutils.c
+++ ch5/strutils.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
 
+#define MAXLEN 1000
 
 void strcatutil(char *s, char *t)
 {
@@ -32,6 +34,13 @@ int strendutil(char *s, char *t)
   return 1;
 }
 
+void strcpyutil(char *s, char *t)
+{
+  while (*t++ = *s++)
+    ;
+  *t = '\0';
+}
+
 void strncpyutil(char *s, char *t, int n)
 {
   char *x = t;
@@ -51,4 +60,60 @@ void strncatutil(char *s, char *t, int n)
     n--;
   }
   *s = '\0';
+}
+
+int strncmputil(char *s, char *t, int n)
+{
+  while( (*s++ == *t++) && --n )
+    ;
+  if (n != 0)
+    return -1;
+  return 0;
+}
+
+int getline_(char *s, int n)
+{
+  char c;
+  int i;
+  i = 0;
+  while ((c = getchar()) != EOF && c != '\n' && n--) {
+    *s++ = c;
+    i++;
+  }
+  if (c == '\n') {
+    *s++ = c;
+    i++;
+  }
+  *s = '\0';
+  return i;
+}
+
+int readlines(char *lineptr[], int maxlines, char *linesbuf, int size)
+{
+  int i, len, total, nlines;
+  char *p, line[MAXLEN];
+  total = 0;
+  nlines = 0;
+  while ((len = getline_(line, MAXLEN)) > 0)
+    if (total > size)
+      return -1;
+    else {
+      if (*(line+len-1) == '\n')
+        *(line+--len) = '\0';
+      strcpyutil(line, linesbuf+total);
+      lineptr[nlines++] = linesbuf+total;
+      total =  total + len + 1;
+      printf("debug: next : %x\n", linesbuf+total);
+    }
+  printf("DEBUG: returning %d\n", nlines);
+  return nlines;
+}
+
+int writelines(char *s[], int n)
+{
+  int i = 0;
+  int j;
+  while (i < n) {
+      printf("%s\n", s[i++]);
+  }
 }
