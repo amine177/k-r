d777eb9a33fcce1ab99e72477f4b15bce1c4d3d0
aminos <amriamine@protonmail.com>
2018-07-12 03:28:57 +0100

    resturctring

--- chap2.4.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(void)
-{
-  int s;
-  s = 56;
-  printf("Ther are %d weeks in a year!\n", s);
-  return 0;
-}
--- format.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-
-int main() {
-  printf("%.1s", "hi", 10);
-  return 0;
-}
