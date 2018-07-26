5a639b79c1b0bdbe321628192e877b9192c8fb28
aminos <amriamine@protonmail.com>
2018-07-26 18:19:12 +0100

    added rectangle structure, addpoint and canonicrect

--- ch6/point.c
+++ ch6/point.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 #include <string.h>
 
+#define min(a, b) ((a) < (b) ? (a) : (b))
+#define max(a, b) ((a) > (b) ? (a) : (b))
 struct point {
   int x;
   int y;
   char name[9];
+};
+
+struct rect {
+  struct point p1;
+  struct point p2;
 };
 
 struct point makepoint(int, int, char[]);
@@ -29,5 +36,29 @@ struct point makepoint(int x, int y, char name[])
   temp.x = x;
   temp.y = y;
   strncpy(temp.name, name, 9);
+  return temp;
+}
+
+struct point addpoint(struct point p1, struct point p2)
+{
+  p1.x += p2.x;
+  p1.y += p2.y;
+  return p1;
+}
+
+int ptinrect(struct point p, struct rect r)
+{
+  return p.x >= r.p1.x && p.x < r.p2.x
+      && p.y >= r.p1.y && p.y < r.p2.y;
+}
+
+struct rect canonicrect(struct rect r)
+{
+  struct rect temp;
+
+  temp.p1.x = min(r.p1.x, r.p2.x);
+  temp.p1.y = min(r.p1.y, r.p2.y);
+  temp.p2.x = max(r.p1.x, r.p2.x);
+  temp.p2.y = max(r.p1.y, r.p2.y);
   return temp;
 }
