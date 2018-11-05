b80136110defea929efa53a370c57b48c52412dd
aminos <amriamine@protonmail.com>
2018-11-06 00:27:00 +0100

    a simple program to show data sizes, not part of the book but goes well with chapter 2

---
+++ ch2/data_types_size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+int main()
+{
+  printf("char: %d\n", sizeof(char));
+  printf("short: %d\n", sizeof(short));
+  printf("int: %d\n", sizeof(int));
+  printf("float: %d\n", sizeof(float));
+  printf("double: %d\n", sizeof(double));
+  printf("long int: %d\n", sizeof(long int));
+  printf("long long: %d\n", sizeof(long long));
+  printf("long double: %d\n", sizeof(long double));
+
+  return 0;
+}
