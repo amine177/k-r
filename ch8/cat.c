6be0bb1876695d8422e235470bd9516a2b29cdd0
aminos <amriamine@protonmail.com>
2019-02-20 18:51:37 +0100

    refreshing

--- ch8/cat.c
+++ ch8/cat.c
@@ -1,50 +1,37 @@
 #include <fcntl.h>
-#include <sys/types.h>
-#include <stdarg.h>
-#include <unistd.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <stdarg.h>
 
 
-void error(char*, ...);
+void error(char *, ...);
 void copy(int, int);
+
 
 int main(int argc, char *argv[])
 {
-  int f1;
+  int fd;
 
-  if (argc == 1)
+  if (argc == 1) {
     copy(0, 1);
-
-  while (--argc) {
-    if ((f1 = open(*++argv, O_RDONLY)) == -1)
-      error("couldn't open file %s", argv);
-    copy(f1, 1);
+    return 0;
+  } else
+    while (--argc > 0) {
+      if ((fd = open(*++argv, O_RDONLY, 0)) == -1)
+        error("can't open file %s", *argv);
+      else {
+        copy(fd, 1);
+        close(fd);
+      }
   }
-
-  return 0;
 }
 
 
-void error(char *fmt, ...)
-{
-  va_list args;
-
-  va_start(args, fmt);
-  fprintf(stderr, "error ");
-  vfprintf(stderr, fmt, args);
-  fprintf(stderr, "\n");
-  va_end(args);
-  _exit(1);
-}
-
-
-void copy(int fd1, int fd2)
+void copy(int src, int dst)
 {
   int n;
   char buf[BUFSIZ];
-  while ((n = read(fd1, buf, sizeof buf)) != 0) {
-    if (n == -1)
-      error("couldn't read file");
-    write(fd2, buf, n);
-  }
+
+  while ((n = read(src, buf, sizeof buf)) > 0)
+    write(dst, buf, n);
 }
