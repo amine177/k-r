78e3d463f81e5689051304182f79328e367b7b2f
Amine Amri <amine.amri@forescout.com>
2023-01-18 17:38:54 +0100

    exe3

---
+++ ch1/exe-1.3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+
+int main(int argc, char* argv[static argc]) {
+
+	printf("Fahr\tCelsius\n");
+	for (int fahr = 0; fahr < 300; fahr += 20) {
+		printf("%d\t%d\n", fahr, 5 * (fahr - 32) / 9);
+	}
+
+	return 0;
+}
