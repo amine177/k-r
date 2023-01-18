6ececc027fe1251820b5d026000c2e7235599f4e
Amine Amri <amine.amri@forescout.com>
2023-01-18 17:50:02 +0100

    exe8

---
+++ ch1/exe-1.8.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+
+
+int main(int argc, char* argv[static argc]) {
+
+
+	char c;
+	int blanks, tabs, newlines;
+	blanks = tabs = newlines = 0;
+	while ((c = getchar()) != EOF) {
+		tabs += (c == '\t');
+		newlines += (c == '\n');
+		blanks += (c == ' ');
+	}
+
+
+	printf("tabs: %d, newlines: %d, tabs: %d\n",
+			tabs,
+			newlines,
+			blanks);
+			
+			
+	return 0;
+}
